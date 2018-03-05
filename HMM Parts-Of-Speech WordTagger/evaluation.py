d614f88d7f338981fefef4d35a7431e4847ac55c
NeeharikaM <33067381+NeeharikaM@users.noreply.github.com>
2018-03-05 09:47:52 -0800

    Create evaluation.py

---
+++ HMM Parts-Of-Speech WordTagger/evaluation.py
@@ -0,0 +1,26 @@
+counter = 0
+total = 0
+
+f1 = open("en_dev_tagged.txt")
+f2 = open("hmmoutput.txt")
+
+data_in = []
+data_out = []
+
+data_in += f1
+data_out += f2
+
+print (len(data_in))
+print (len(data_out))
+
+for i in range(0, len(data_in)):
+	l1 = data_in[i].split()
+	l2 = data_out[i].split()
+	for j in range(0, len(l1)):
+		if l1[j] == l2[j]:
+			counter += 1
+		total += 1
+
+print (total)
+print (counter)
+print (float(counter * 100)/total)
