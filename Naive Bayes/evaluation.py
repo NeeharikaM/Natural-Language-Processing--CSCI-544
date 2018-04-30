7261a88b4c6238d4bd363e9219f1e4fc4a269be8
NeeharikaM <33067381+NeeharikaM@users.noreply.github.com>
2018-04-30 16:51:17 -0700

    Add files via upload

---
+++ Naive Bayes/evaluation.py
@@ -0,0 +1,18 @@
+f1 = open("dev-key.txt")
+f2 = open("nboutput.txt")
+
+counter = 0
+
+data_in = []
+data_out = []
+
+data_in += f1
+data_out += f2
+
+for i in range(0, len(data_in)):
+	l1 = data_in[i].split()
+	l2 = data_out[i].split()
+	if(l1[1]==l2[1] and l1[2]==l2[2]):
+		counter += 1
+print (len(data_in))
+print (counter)
